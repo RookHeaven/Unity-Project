84c7ae5b280e4b486d93ee1f31873cd5f62ebfdc
RookHeaven <romagra92@yandex.ru>
2020-10-15 01:06:36 +0500

    добавил слежение камерой за игроком

---
+++ Assets/My Game/Scripts/Homework 5/CameraScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject Player;
+
+    private Vector3 offset;
+    private void Start()
+    {
+        offset = transform.position - Player.transform.position;
+    }
+
+    private void Update()
+    {
+        transform.position = Player.transform.position + offset;
+    }
+}
