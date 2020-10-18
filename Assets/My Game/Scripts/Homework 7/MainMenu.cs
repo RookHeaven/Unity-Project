d3da0a9b0b9bcee495db99084dfb75d9e8d6275c
RookHeaven <romagra92@yandex.ru>
2020-10-18 16:03:55 +0500

    добавил меню и меню паузы

---
+++ Assets/My Game/Scripts/Homework 7/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    #region Methods
+
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 0);
+    }
+
+    public void QuitGame ()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+
+    #endregion
+}
---
+++ Assets/My Game/Scripts/Homework 7/PauseMenu.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PauseMenu : MonoBehaviour
+{
+    #region Fields
+
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    #endregion
+
+
+    #region UnityMethods
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    #endregion
+
+    #region Methods
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1.0f;
+        GameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0.0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game");
+        Application.Quit();
+    }
+
+    #endregion
+}
